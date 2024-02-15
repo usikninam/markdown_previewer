8dc521b1403c2d87b79a0492a47a4431f46a6dba
usikninam <usikninam@gmail.com>
2024-02-15 20:59:38 +0100

    Default text fulfill requirements

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Preview from './Preview';
 
 function App() {
 
+  const multilineCode = "```\n// a code block:\nfunction anotherExample(firstLine, lastLine){\n if (firstLine == '```' && lastLine == '```'){\n  return multiLineCode;\n }\n}\n```\n";
 
-  // a header (H1 size)
-  // a sub header (H2 size)
-  // a link
-  // inline code
-  // a code block
-  // a list item
-  // a blockquote
-  // an image,
-  // bolded text
-  const defaultText = "# Heading\n*italic*\n**bold**\n### heading3\n```code```";
+  const defaultText = "# a header (H1 size)\n" +
+    "## a sub header (H2 size)\n" +
+    "[a link](https://google.com)\n" +
+    "some `int answer = 42;` inline code \n" +
+    multilineCode +
+    "- a list item\n" +
+    ">a blockquote\n" +
+    "![an image](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n" +
+    "**bolded text**\n";
 
   const [text, setText] = useState(defaultText);
 
