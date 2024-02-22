5c4ed8d5569661d7dd2f199a6c1505b0618d11e9
usikninam <usikninam@gmail.com>
2024-02-22 21:06:35 +0100

    Add marked option to interpret line break as <br>

--- src/Preview.js
+++ src/Preview.js
@@ -2,6 +2,12 @@ import { marked } from 'marked';
 
 export default function Preview(props) {
 
+    // Set options https://marked.js.org/using_advanced
+    marked.use({
+        gfm: true,
+        breaks: true
+    });
+
     return (
         <div id="preview" dangerouslySetInnerHTML={{ __html: marked(props.text) }}></div>
     );
